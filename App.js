93b3fc4e98eb2474b61dfae0cdf52ae64f215d34
Arij Saleem <arijsaleem@gmail.com>
2020-01-26 04:34:26 -0500

    added button

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+    Image,
+    StyleSheet,
+    Text,
+    View,
+    Button,
+    Alert,
+    SafeAreaView,
+} from 'react-native';
+import logo from './assets/wasteman_logo.png';
+import { SplashScreen } from 'expo';
+import Constants from 'expo-constants';
+import AwesomeButton from "react-native-really-awesome-button";
+
+function Separator() {
+    return <View style={styles.separator} />;
+}
+
+function ScanButton() {
+    return (
+            <AwesomeButton raiseLevel={0} >
+        Scan an item
+     </AwesomeButton>
+    );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+        <View>
+          <Image source={logo} style={styles.logo} />
+        </View>
+        <Separator />
+        <View>
+          <ScanButton />
+        </View>
+    </SafeAreaView>
   );
 }
 
@@ -15,5 +45,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 10,
+    resizeMode:'contain'
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#fff',
+    borderBottomWidth: StyleSheet.hairlineWidth,
   },
 });
